567db240acc3b5d518e24a66c000c4287bf1b1c3
Amit Duwal <amitduwal1098@gmail.com>
2020-05-30 20:23:39 +0545

    Content Management System

--- schema.php
+++ schema.php
@@ -26,9 +26,62 @@
 					password = '".sha1('admin@magazine.comadmin123')."',
 					role = 'Admin',
 					status = 'Active'	
+			",
+			'category' => "
+				CREATE TABLE IF NOT EXISTS categories
+					(
+						id int not null AUTO_INCREMENT PRIMARY KEY,
+						categoryname varchar(30),
+						description text,
+						status enum('Active','Passive') default 'Active',
+						added_by int,
+						created_date datetime default current_timestamp,
+						updated_date datetime on update current_timestamp
+					)
+			",
+			'blog-post' => "
+				CREATE TABLE IF NOT EXISTS blogs
+					(
+						id int not null AUTO_INCREMENT PRIMARY KEY,
+						
+						title varchar(250),
+						content text,
+						featured enum('Featured','notFeatured') default 'notFeatured',
+						categoryid int,
+						view int,
+						image varchar(50),
+						status enum('Active','Passive') default 'Active',
+						added_by int,
+						created_date datetime default current_timestamp,
+						updated_date datetime on update current_timestamp
+					)
+					",
+					'advertisement'=>"
+						CREATE TABLE IF NOT EXISTS advertisements
+							(
+								id int not null AUTO_INCREMENT PRIMARY KEY,
+								url varchar(250),
+								type enum('Simple','Wide') default 'Simple',
+								status enum('Active','Passive') default 'Passive',
+								image varchar(50),
+								added_by int,
+								created_date datetime default current_timestamp,
+								updated_date datetime on update current_timestamp
+							)
+					",
+					'followus'=>"
+						CREATE TABLE IF NOT EXISTS followuss
+							(
+								id int not null AUTO_INCREMENT PRIMARY KEY,
+								iconname varchar(20),
+								url text,
+								status enum('Active','Passive') default 'Passive',
+								added_by int,
+								created_date datetime default current_timestamp,
+								updated_date datetime on update current_timestamp
+							)
 			"
 		);
-
 	foreach ($table as $key => $sql) {
 		try{
 			$success = $schema->create($sql);
@@ -41,9 +94,5 @@
 		}catch(PDOException $e){
 			error_log(Date("M d, Y h:i:s a").' : (run Query) : '.$e->getMessage()."\r\n",3,ERROR_PATH.'error.log');
 			return false;
-
 		}
 	}
-
-
- ?>
